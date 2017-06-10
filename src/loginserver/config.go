1e6bb5fef79a8624cb12be20fd2c2a5686d6ecd7
ubis <ubitas52@gmail.com>
2017-06-10 13:42:04 +0300

    Implemented conf reader

--- src/loginserver/config.go
+++ src/loginserver/config.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"strconv"
-	"share/lib/goini"
 	"share/directory"
+	"share/conf"
 )
 
 // Default values
@@ -14,7 +13,6 @@ const (
 
 // Configuration struct
 type Config struct {
-	file 		*goini.INI
 	Port		int
 	MaxUsers	int
 }
@@ -24,65 +22,14 @@ func (c *Config) Read() {
 	log.Info("Reading configuration...")
 
 	var location = directory.Root() + "/cfg/loginserver.ini"
-	var ini 	 = goini.New()
 
 	// parse configuration file...
-	if err := ini.ParseFile(location); err != nil {
+	if err := conf.Open(location); err != nil {
 		log.Fatalf("Couldn't read configuration file %s. %s", location, err.Error())
 		return
 	}
 
-	c.file = ini
-
 	// read values from configuration...
-	c.Port, _     = c.getInt("network", "port", C_Port)
-	c.MaxUsers, _ = c.getInt("server", "max_users", C_MaxUsers)
-}
-
-/*
-	Gets value from configuration file, if section or key isn't found,
-	default value will be returned
-	@param 	section 	conf section
-	@param	key			conf key
-	@param	def			default value
-	@return	string value, either from conf or default and error, if any
- */
-func (c *Config) getString(section string, key string, def string) (string, error) {
-	if value, ok := c.file.SectionGet(section, key); ok != false {
-		return value, nil
-	}
-
-	return def, nil
-}
-
-/*
-	Gets value from configuration file, if section or key isn't found,
-	default value will be returned
-	@param 	section 	conf section
-	@param	key			conf key
-	@param	def			default value
-	@return	int value, either from conf or default and error, if any
- */
-func (c *Config) getInt(section string, key string, def int) (int, error) {
-	if value, ok := c.file.SectionGet(section, key); ok != false {
-		return strconv.Atoi(value)
-	}
-
-	return def, nil
-}
-
-/*
-	Gets value from configuration file, if section or key isn't found,
-	default value will be returned
-	@param 	section 	conf section
-	@param	key			conf key
-	@param	def			default value
-	@return	bool value, either from conf or default and error, if any
- */
-func (c *Config) getBool(section string, key string, def bool) (bool, error) {
-	if value, ok := c.file.SectionGet(section, key); ok != false {
-		return strconv.ParseBool(value)
-	}
-
-	return def, nil
+	c.Port	   = conf.GetInt("network", "port", C_Port)
+	c.MaxUsers = conf.GetInt("server", "max_users", C_MaxUsers)
 }

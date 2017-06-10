78a537187296750d3b61f45ebdba25eb54e6f5a4
ubis <ubitas52@gmail.com>
2017-06-10 14:01:19 +0300

    Changed tabs to spaces

--- src/loginserver/config.go
+++ src/loginserver/config.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-	"share/directory"
-	"share/conf"
+    "share/directory"
+    "share/conf"
 )
 
 // Default values
 const (
-	C_Port 	 	= 38101
-	C_MaxUsers 	= 100
+    C_Port     = 38101
+    C_MaxUsers = 100
 )
 
 // Configuration struct
 type Config struct {
-	Port		int
-	MaxUsers	int
+    Port        int
+    MaxUsers    int
 }
 
 // Attempts to read server configuration file
 func (c *Config) Read() {
-	log.Info("Reading configuration...")
+    log.Info("Reading configuration...")
 
-	var location = directory.Root() + "/cfg/loginserver.ini"
+    var location = directory.Root() + "/cfg/loginserver.ini"
 
-	// parse configuration file...
-	if err := conf.Open(location); err != nil {
-		log.Fatalf("Couldn't read configuration file %s. %s", location, err.Error())
-		return
-	}
+    // parse configuration file...
+    if err := conf.Open(location); err != nil {
+        log.Fatalf("Couldn't read configuration file %s. %s", location, err.Error())
+        return
+    }
 
-	// read values from configuration...
-	c.Port	   = conf.GetInt("network", "port", C_Port)
-	c.MaxUsers = conf.GetInt("server", "max_users", C_MaxUsers)
+    // read values from configuration...
+    c.Port     = conf.GetInt("network", "port", C_Port)
+    c.MaxUsers = conf.GetInt("server", "max_users", C_MaxUsers)
 }

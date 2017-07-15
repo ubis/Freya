326202c1552fc57e7acda6395d591a2731a1461e
ubis <ubitas52@gmail.com>
2017-07-15 13:33:05 +0300

    Add url to client packet

--- src/loginserver/def/config.go
+++ src/loginserver/def/config.go
@@ -16,10 +16,17 @@ const (
 
 // Configuration struct
 type Config struct {
-    Port        int
-    Version     int
-    MasterIp    string
-    MasterPort  int
+    Port               int
+
+    Version            int
+    CashWeb_URL        string
+    CashWeb_Odc_URL    string
+    CashWeb_Charge_URL string
+    GuildWeb_URL       string
+    Sns_URL            string
+
+    MasterIp           string
+    MasterPort         int
 }
 
 // Attempts to read server configuration file
@@ -35,8 +42,15 @@ func (c *Config) Read() {
     }
 
     // read values from configuration...
-    c.Port       = conf.GetInt("network", "port", C_Port)
-    c.Version    = conf.GetInt("client", "client_version", C_Version)
+    c.Port = conf.GetInt("network", "port", C_Port)
+
+    c.Version            = conf.GetInt("client", "client_version", C_Version)
+    c.CashWeb_URL        = conf.GetString("client", "cashweb_url", "")
+    c.CashWeb_Odc_URL    = conf.GetString("client", "cashweb_odc_url", "")
+    c.CashWeb_Charge_URL = conf.GetString("client", "cashweb_charge_url", "")
+    c.GuildWeb_URL       = conf.GetString("client", "guildweb_url", "")
+    c.Sns_URL            = conf.GetString("client", "sns_url", "")
+
     c.MasterIp   = conf.GetString("master", "ip", C_MasterIp)
     c.MasterPort = conf.GetInt("master", "port", C_MasterPort)
 }
--- src/loginserver/packet/auth.go
+++ src/loginserver/packet/auth.go
@@ -58,8 +58,6 @@ func AuthAccount(session *network.Session, reader *network.Reader) {
     var userId = string(bytes.Trim(data[:32], "\x00"))
     var userPw = string(bytes.Trim(data[32:], "\x00"))
 
-    log.Infof("AuthRequest with id `%s`", userId)
-
     var r = account.AuthResponse{Status: account.None}
     err = g_RPCHandler.Call(rpc.AuthCheck,
         account.AuthRequest{userId, userPw}, &r)
@@ -85,7 +83,44 @@ func AuthAccount(session *network.Session, reader *network.Reader) {
 
     if r.Status == account.Normal {
         log.Infof("User `%s` succesfully logged in.", userId)
+
+        // send url's
+        URLToClient(session)
+    } else {
+        log.Infof("User `%s` failed to log in.", userId)
     }
+
+    session.Send(packet)
+}
+
+// URLToClient Packet which is NFY
+func URLToClient(session *network.Session) {
+    var cash_url        = g_ServerConfig.CashWeb_URL
+    var cash_odc_url    = g_ServerConfig.CashWeb_Odc_URL
+    var cash_charge_url = g_ServerConfig.CashWeb_Charge_URL
+    var guildweb_url    = g_ServerConfig.GuildWeb_URL
+    var sns_url         = g_ServerConfig.Sns_URL
+
+    var dataLen = len(cash_url) + 4
+    dataLen += len(cash_odc_url) + 4
+    dataLen += len(cash_charge_url) + 4
+    dataLen += len(guildweb_url) + 4
+    dataLen += len(sns_url) + 4
+
+    var packet = network.NewWriter(URLTOCLIENT)
+    packet.WriteInt16(dataLen + 2)
+    packet.WriteInt16(dataLen)
+
+    packet.WriteInt32(len(cash_url))
+    packet.WriteString(cash_url)
+    packet.WriteInt32(len(cash_odc_url))
+    packet.WriteString(cash_odc_url)
+    packet.WriteInt32(len(cash_charge_url))
+    packet.WriteString(cash_charge_url)
+    packet.WriteInt32(len(guildweb_url))
+    packet.WriteString(guildweb_url)
+    packet.WriteInt32(len(sns_url))
+    packet.WriteString(sns_url)
 
     session.Send(packet)
 }

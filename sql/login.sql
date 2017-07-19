aff232a15ed7899e6b7deeaf645608b5f414a921
ubis <ubitas52@gmail.com>
2017-07-19 16:53:06 +0300

    Update login database

--- sql/login.sql
+++ sql/login.sql
@@ -10,7 +10,7 @@ Target Server Type    : MariaDB
 Target Server Version : 100121
 File Encoding         : 65001
 
-Date: 2017-07-14 20:38:18
+Date: 2017-07-19 16:52:01
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -32,3 +32,17 @@ CREATE TABLE `accounts` (
 -- Records of accounts
 -- ----------------------------
 INSERT INTO `accounts` VALUES ('1', 'ubis', '$2a$10$.xhBdq6h30zS533vogmFpeUedBbbEbYcS6a4ldg/0fwXuj.CzBSHe', '32', 'xEQrKqsUuVDxmTdVCMMjFygOC5TxxGvX');
+
+-- ----------------------------
+-- Table structure for accounts_subpassword
+-- ----------------------------
+DROP TABLE IF EXISTS `accounts_subpassword`;
+CREATE TABLE `accounts_subpassword` (
+  `account` int(11) NOT NULL,
+  `password` char(60) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `answer` char(60) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `question` tinyint(3) unsigned NOT NULL,
+  `expires` datetime NOT NULL,
+  PRIMARY KEY (`account`),
+  CONSTRAINT `accounts_subpassword_ibfk_1` FOREIGN KEY (`account`) REFERENCES `accounts` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;

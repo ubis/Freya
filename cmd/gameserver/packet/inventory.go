321f6135aea673329f693ef680d9add384e654ad
Artem Alekseev <lexx73652@gmail.com>
2023-08-10 13:14:24 +0400

    Refactoring inventory.go

--- cmd/gameserver/packet/inventory.go
+++ cmd/gameserver/packet/inventory.go
@@ -1,11 +1,20 @@
 package packet
 
 import (
+	"github.com/ubis/Freya/share/log"
 	"github.com/ubis/Freya/share/models/character"
 	"github.com/ubis/Freya/share/network"
 	"github.com/ubis/Freya/share/rpc"
-	"log"
 )
+
+func MoveItemMasterServerQuery(packet string, id int32, deleteSlot uint16, createSlot uint16) {
+	send := character.ItemEquipReq{Server: byte(g_ServerSettings.ServerId), Id: id, DeleteSlot: deleteSlot, CreateSlot: createSlot}
+	receive := character.ItemEquipRes{}
+	err := g_RPCHandler.Call(packet, send, &receive)
+	if err != nil {
+		log.Error(err.Error())
+	}
+}
 
 func MoveItem(session *network.Session, reader *network.Reader) {
 	//EQUIP -> INVENTORY
@@ -24,46 +33,40 @@ func MoveItem(session *network.Session, reader *network.Reader) {
 	//01 00 00 00 08 00 00 00 01 00 00 00 09 00 00 00 00 00 18 0B EB FF FF FF 00 00 00 00
 	//01 00 00 00 09 00 00 00 01 00 00 00 12 00 00 00 00 00 18 0B EB FF FF FF 00 00 00 00
 
-	//log.Printf("%X", reader.ReadBytes(28))
+	//log.Debugf("%X", reader.ReadBytes(28))
 
 	equip := reader.ReadUint32()
 	deleteSlot := reader.ReadUint32()
 	inventory := reader.ReadInt32()
 	createSlot := reader.ReadUint32()
 
-	var packet = network.NewWriter(MOVE_ITEM)
+	packet := network.NewWriter(MOVE_ITEM)
 
 	packet.WriteByte(0x01)
 	packet.WriteInt64(0)
 
 	session.Send(packet)
 
+	//Need add Notify packets
+
 	id, err := getSessionCharId(session)
 
 	if err != nil {
-		log.Printf(err.Error())
+		log.Error(err.Error())
 		return
 	}
 
 	if equip > 0 {
 		if inventory > 0 {
-			var send = character.ItemEquipReq{byte(g_ServerSettings.ServerId), id, uint16(deleteSlot), uint16(createSlot)}
-			var recv = character.ItemEquipRes{}
-			g_RPCHandler.Call(rpc.MoveItemEquToEqu, send, &recv)
+			MoveItemMasterServerQuery(rpc.MoveItemEquToEqu, id, uint16(deleteSlot), uint16(createSlot))
 		} else {
-			var send = character.ItemEquipReq{byte(g_ServerSettings.ServerId), id, uint16(deleteSlot), uint16(createSlot)}
-			var recv = character.ItemEquipRes{}
-			g_RPCHandler.Call(rpc.MoveItemEquToInv, send, &recv)
+			MoveItemMasterServerQuery(rpc.MoveItemEquToInv, id, uint16(deleteSlot), uint16(createSlot))
 		}
 	} else {
 		if inventory > 0 {
-			var send = character.ItemEquipReq{byte(g_ServerSettings.ServerId), id, uint16(deleteSlot), uint16(createSlot)}
-			var recv = character.ItemEquipRes{}
-			g_RPCHandler.Call(rpc.MoveItemInvToEqu, send, &recv)
+			MoveItemMasterServerQuery(rpc.MoveItemInvToEqu, id, uint16(deleteSlot), uint16(createSlot))
 		} else {
-			var send = character.ItemEquipReq{byte(g_ServerSettings.ServerId), id, uint16(deleteSlot), uint16(createSlot)}
-			var recv = character.ItemEquipRes{}
-			g_RPCHandler.Call(rpc.MoveItemInvToInv, send, &recv)
+			MoveItemMasterServerQuery(rpc.MoveItemInvToInv, id, uint16(deleteSlot), uint16(createSlot))
 		}
 	}
 }

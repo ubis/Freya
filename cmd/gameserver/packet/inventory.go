c5322e0f5d4783dbc6f033c86c102f6cbc41a04e
Artem Alekseev <lexx73652@gmail.com>
2023-08-12 01:28:57 +0400

    Refactoring

--- cmd/gameserver/packet/inventory.go
+++ cmd/gameserver/packet/inventory.go
@@ -3,11 +3,12 @@ package packet
 import (
 	"github.com/ubis/Freya/share/log"
 	"github.com/ubis/Freya/share/models/character"
+	"github.com/ubis/Freya/share/models/inventory"
 	"github.com/ubis/Freya/share/network"
 	"github.com/ubis/Freya/share/rpc"
 )
 
-func SendStorageExchangeMoveStatusPacket(session *network.Session, result byte) {
+func NotifyStorageExchange(session *network.Session, result byte) {
 	packet := network.NewWriter(STORAGE_EXCHANGE_MOVE)
 	packet.WriteByte(result)
 	packet.WriteInt32(0)
@@ -15,15 +16,34 @@ func SendStorageExchangeMoveStatusPacket(session *network.Session, result byte) 
 	session.Send(packet)
 }
 
-func SendStorageExchangeMoveMasterServerPacket(packet string, id int32, deleteSlot uint16, createSlot uint16) character.ItemMoveRes {
-	send := character.ItemMoveReq{Server: byte(g_ServerSettings.ServerId), Id: id, DeleteSlot: deleteSlot, CreateSlot: createSlot}
-	receive := character.ItemMoveRes{}
-	err := g_RPCHandler.Call(packet, send, &receive)
+func UpdatePlayerStorage(session *network.Session, packet string, deleteSlot uint16, createSlot uint16) (inventory.Item, error) {
+	id, err := getSessionCharId(session)
 	if err != nil {
-		log.Error(err.Error())
+		return inventory.Item{}, err
 	}
 
-	return receive
+	send := character.ItemMoveReq{Server: byte(g_ServerSettings.ServerId), Id: id, DeleteSlot: deleteSlot, CreateSlot: createSlot}
+	receive := character.ItemMoveRes{}
+	err = g_RPCHandler.Call(packet, send, &receive)
+	if err != nil {
+		return inventory.Item{}, err
+	}
+
+	if receive.Result != nil {
+		return inventory.Item{}, receive.Result
+	}
+
+	ctx, err := parseSessionContext(session)
+	if err != nil {
+		return inventory.Item{}, err
+	}
+
+	ctx.mutex.Lock()
+	ctx.char.Equipment.Remove(deleteSlot)
+	ctx.char.Inventory.Set(createSlot, receive.Item)
+	ctx.mutex.Unlock()
+
+	return receive.Item, nil
 }
 
 func StorageExchangeMove(session *network.Session, reader *network.Reader) {
@@ -32,99 +52,92 @@ func StorageExchangeMove(session *network.Session, reader *network.Reader) {
 	inventory := reader.ReadUint32()
 	createSlot := uint16(reader.ReadUint32())
 
-	ctx, err := parseSessionContext(session)
-	if err != nil {
-		log.Error(err.Error())
-		SendStorageExchangeMoveStatusPacket(session, 0x00)
-		return
-	}
-	playerChar := ctx.char
-
 	isEquip := equip == 1
 	isInventory := inventory == 1
+	var err error = nil
 
 	if isEquip && !isInventory {
-		result := SendStorageExchangeMoveMasterServerPacket(rpc.UnEquipItem, playerChar.Id, deleteSlot, createSlot)
-
-		if result.Result == nil {
-			playerChar.Equipment.Remove(deleteSlot)
-			//playerChar.Inventory.Set(createSlot, result.Item)
-
-			pkt := network.NewWriter(NFY_ITEM_UNEQUIP)
-			pkt.WriteInt32(playerChar.Id)
-			pkt.WriteInt16(deleteSlot)
-
-			g_NetworkManager.SendToAll(pkt)
-
-			SendStorageExchangeMoveStatusPacket(session, 0x01)
-			return
-		} else {
-			log.Error(result.Result)
-			SendStorageExchangeMoveStatusPacket(session, 0x00)
+		_, updateErr := UpdatePlayerStorage(session, rpc.UnEquipItem, deleteSlot, createSlot)
+		if updateErr != nil {
+			log.Error(err)
+			NotifyStorageExchange(session, 0x00)
 			return
 		}
+
+		id, err2 := getSessionCharId(session)
+		if err2 != nil {
+			log.Error(err2.Error())
+			NotifyStorageExchange(session, 0x00)
+			return
+		}
+
+		pkt := network.NewWriter(NFY_ITEM_UNEQUIP)
+		pkt.WriteInt32(id)
+		pkt.WriteInt16(deleteSlot)
+		g_NetworkManager.SendToAll(pkt)
+
+		NotifyStorageExchange(session, 0x01)
+
+		return
 	}
 
 	if isInventory && !isEquip {
-		result := SendStorageExchangeMoveMasterServerPacket(rpc.EquipItem, playerChar.Id, deleteSlot, createSlot)
-
-		if result.Result == nil {
-			playerChar.Inventory.Remove(deleteSlot)
-			playerChar.Equipment.Set(createSlot, result.Item)
-
-			pkt := network.NewWriter(NFY_ITEM_EQUIP)
-			pkt.WriteInt32(playerChar.Id)
-			pkt.WriteInt32(result.Item.Kind)
-			pkt.WriteInt16(result.Item.Slot)
-			pkt.WriteByte(0x00)
-			pkt.WriteByte(0x00)
-			pkt.WriteByte(0x00)
-			pkt.WriteByte(0x00)
-			pkt.WriteByte(0x00)
-
-			g_NetworkManager.SendToAll(pkt)
-
-			SendStorageExchangeMoveStatusPacket(session, 0x01)
-			return
-		} else {
-			log.Error(result.Result)
-			SendStorageExchangeMoveStatusPacket(session, 0x00)
+		item, updateErr := UpdatePlayerStorage(session, rpc.EquipItem, deleteSlot, createSlot)
+		if updateErr != nil {
+			log.Error(updateErr)
+			NotifyStorageExchange(session, 0x00)
 			return
 		}
+
+		id, charIdErr := getSessionCharId(session)
+		if charIdErr != nil {
+			log.Error(charIdErr.Error())
+			NotifyStorageExchange(session, 0x00)
+			return
+		}
+
+		pkt := network.NewWriter(NFY_ITEM_EQUIP)
+		pkt.WriteInt32(id)
+		pkt.WriteInt32(item.Kind)
+		pkt.WriteInt16(item.Slot)
+		pkt.WriteByte(0x00)
+		pkt.WriteByte(0x00)
+		pkt.WriteByte(0x00)
+		pkt.WriteByte(0x00)
+		pkt.WriteByte(0x00)
+
+		g_NetworkManager.SendToAll(pkt)
+
+		NotifyStorageExchange(session, 0x01)
+		return
 	}
 
 	if isEquip && isInventory {
-		result := SendStorageExchangeMoveMasterServerPacket(rpc.ChangeEquipItemSlot, playerChar.Id, deleteSlot, createSlot)
-		if result.Result == nil {
-			playerChar.Equipment.Remove(deleteSlot)
-			playerChar.Equipment.Set(createSlot, result.Item)
+		_, updateErr := UpdatePlayerStorage(session, rpc.ChangeEquipItemSlot, deleteSlot, createSlot)
+		if updateErr != nil {
+			log.Error(updateErr)
+			NotifyStorageExchange(session, 0x00)
+			return
 
 			//TODO Check and add notify packets
-
-			SendStorageExchangeMoveStatusPacket(session, 0x01)
-			return
-		} else {
-			log.Error(result.Result)
-			SendStorageExchangeMoveStatusPacket(session, 0x00)
-			return
 		}
+
+		NotifyStorageExchange(session, 0x01)
+		return
 	}
 
 	if !isEquip && !isInventory {
-		result := SendStorageExchangeMoveMasterServerPacket(rpc.ChangeInventoryItemSlot, playerChar.Id, uint16(deleteSlot), uint16(createSlot))
+		_, updateErr := UpdatePlayerStorage(session, rpc.ChangeInventoryItemSlot, uint16(deleteSlot), uint16(createSlot))
 
-		if result.Result == nil {
-			playerChar.Inventory.Remove(deleteSlot)
-			//playerChar.Inventory.Set(createSlot, result.Item)
-
-			SendStorageExchangeMoveStatusPacket(session, 0x01)
-			return
-		} else {
-			log.Error(result.Result)
-			SendStorageExchangeMoveStatusPacket(session, 0x00)
+		if updateErr != nil {
+			log.Error(updateErr)
+			NotifyStorageExchange(session, 0x00)
 			return
 		}
+
+		NotifyStorageExchange(session, 0x01)
+		return
 	}
 
-	SendStorageExchangeMoveStatusPacket(session, 0x00)
+	NotifyStorageExchange(session, 0x00)
 }

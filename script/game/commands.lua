165512f74cce758d385013bc3bdef2d8ea14db75
Jokubas Maciulaitis <ubitas52@gmail.com>
2023-08-19 22:33:36 +0300

    gameserver: add #drop command which drops an item specified by kind and option values This is just one of the examples of how to write raw packets to the client.

--- script/game/commands.lua
+++ script/game/commands.lua
@@ -25,3 +25,60 @@ addCommandHandler('setlevel', function(session, arg)
     setPlayerLevel(session, level)
     sendClientMessage(session, 'Your level is set to '..level)
 end)
+
+addCommandHandler('drop', function(session, kind, opt)
+    local kind_id = tonumber(kind)
+    local opt_id = tonumber(opt)
+
+    if not kind_id or not opt_id then
+        sendClientMessage(session, 'Invalid command usage: #drop <kind> <opt>')
+        return
+    end
+
+    local x, y = getPlayerPosition(session)
+    local opcode = 204 -- NewItemList packet opcode
+    local item_id = math.random(1, 100) -- use random
+
+    -- some functions to fill buffer
+    local function band(a, b)
+        local result = 0
+        local bitval = 1
+        while a > 0 and b > 0 do
+            if a % 2 == 1 and b % 2 == 1 then
+                result = result + bitval
+            end
+            a = math.floor(a / 2)
+            b = math.floor(b / 2)
+            bitval = bitval * 2
+        end
+        return result
+    end
+
+    local function rshift(a, n)
+        return math.floor(a / 2^n)
+    end
+
+    local function append(tbl, n, count)
+        count = count or 4
+        for i = 1, count do
+            table.insert(tbl, band(rshift(n, (i - 1) * 8), 0xFF))
+        end
+    end
+
+    local bytes = {}
+
+    table.insert(bytes, 0x01) -- count
+    append(bytes, item_id)    -- item idx
+    append(bytes, opt)        -- item opt
+    append(bytes, 0x0A000602) -- from idx
+    append(bytes, kind)       -- kind Idx
+    table.insert(bytes, x)    -- pos x
+    table.insert(bytes, 0x00) --
+    table.insert(bytes, y)    -- pos y
+    table.insert(bytes, 0x00) --
+    append(bytes, 0x330d, 2)  -- uniq key
+    table.insert(bytes, 0x01) -- type
+    table.insert(bytes, 0x04) -- unk
+
+    sendClientPacket(session, opcode, bytes)
+end)
